c6a91983e81af7b01355c4b9644975ca19b09626
FR_Jess <fr_jess@icloud.com>
2022-11-02 16:17:36 +0100

    [add] firstMessage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-const { createApp} = Vue;
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Utilizzare almeno una classe CSS in modo dinamico
+// Bonus:
+// 1. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// 2. Cambiare dinamicamente l’immagine
+// 3. Al click di un bottone nascondere l’immagine e mostrare un testo
+
+const { createApp } = Vue;
 
 createApp({
-  
-})
+
+  data(){
+    return {
+      firstMessage: "Questo è il mio primo esercizio con Vue",
+    }
+  },
+
+}).mount('#app')
